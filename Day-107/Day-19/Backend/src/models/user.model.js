3b3f605c366974562b752f59518b2b1afab785b6
Mannu57 <9122manoranjan@gmail.com>
2026-03-17 20:36:38 +0530

    insta clone -Frantend intigretion

---
+++ Day-107/Day-19/Backend/src/models/user.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: [true, 'User name already exists'],
+    required: [true, 'User name is required'],
+  },
+  email: {
+    type: String,
+    unique: [true, 'Email already exists'],
+    required: [true, 'Email is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is Required'],
+  },
+  bio: String,
+  profile_image: {
+    type: String,
+    default: "https://ik.imagekit.io/Manu01/cohort-2.O/default_image.png"
+  },
+  follower: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  }],
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  }],
+});
+
+const userModel = mongoose.model("users", userSchema)
+
+module.exports = userModel
---
+++ Day-107/Day-19/Backend/src/routes/auth.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authController = require('../controllers/auth.controller')
+const identifyUser = require('../middleware/auth.middleware')
+
+const authRouter = express.Router();
+
+/**
+ * - POST api/auth/register
+ */
+authRouter.post('/register', authController.registerController );
+
+/**
+ * POST api/auth/login
+*/
+
+authRouter.post('/login', authController.loginController)
+
+module.exports = authRouter;
